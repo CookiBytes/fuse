37961daf24fb82070d29c80d33814450de1f141c
Zoe Koh <eden11@protonmail.com>
2020-09-21 14:58:08 +1000

    CSS Files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.css";
+import "./css/style.css";
+import "./css/todo.css";
+import "./css/btns.css";
+import "./css/form.css";
+import "./css/select.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>asdf</h1>
     </div>
   );
 }
