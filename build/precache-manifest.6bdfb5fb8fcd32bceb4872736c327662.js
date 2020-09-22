66a606ff0ce4f2bf1a14bf4bfa2f31b52f420aa6
Zoe Koh <eden11@protonmail.com>
2020-09-22 19:49:28 +1000

    Add images and the build folder

---
+++ build/precache-manifest.6bdfb5fb8fcd32bceb4872736c327662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bca52f385a4b5d64414ed930c6a097f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7ecbf340dbb8e3d2cf1",
+    "url": "/static/css/main.bcafcad9.chunk.css"
+  },
+  {
+    "revision": "018c85179c69e9fd114e",
+    "url": "/static/js/2.1364e5ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1364e5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ecbf340dbb8e3d2cf1",
+    "url": "/static/js/main.ac71d5b0.chunk.js"
+  },
+  {
+    "revision": "191ae04d138be18b3ed5",
+    "url": "/static/js/runtime-main.0420d5e9.js"
+  }
+]);
